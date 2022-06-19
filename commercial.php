209eeccebf984bb9b542d92e54d19b3fb6aa99e2
therealmensumit <sk03950@gmail.com>
2022-06-19 17:06:08 +0530

    content changes

--- commercial.php
+++ commercial.php
@@ -1,135 +1,261 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include('include/head.php')?>
+
 <body>
-<?php include('include/header.php')?>
+  <?php include('include/header.php')?>
 
-    <section class="banner-sec">
-        <img class="desk" src="images/banner2.jpg" alt="" />
-        <img class="mob" src="images/mob-banner-2.jpg" alt="" />
-        <div class="banner-content">
-            <!-- <h2>PROVIDE QUALITY<br />REPAIR SERVICE</h2> -->
-            <h2>AIR CONDITION</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ad eum doloribus earum tempore.</p>
-            
+  <section class="banner-sec">
+    <img class="desk" src="images/banner2.jpg" alt="" />
+    <img class="mob" src="images/mob-banner-2.jpg" alt="" />
+    <div class="banner-content">
+      <!-- <h2>PROVIDE QUALITY<br />REPAIR SERVICE</h2> -->
+      <h2>AIR CONDITION</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ad eum doloribus earum tempore.</p>
+
+    </div>
+  </section>
+
+  <!-- <section class="safe_side_main">
+    <div class="container">
+
+      <div class="safe_menu">
+        <h2>Best-in-class safety measures</h2>
+        <div class="work_on_progress ">
+          <div class="work-progress">
+            <div class="work-icon-box ">
+              <span><img src="images/services/air/masks.png"></span>
+            </div>
+            <h3 class="caption">Usage of masks &amp; gloves</h3>
+          </div>
+          <div class="work-arrow">
+          </div>
+          <div class="work-progress">
+            <div class="work-icon-box">
+              <span><img src="images/services/air/tempreture.png"></span>
+            </div>
+            <h3 class="caption">Tempreture checks</h3>
+          </div>
+          <div class="work-arrow">
+          </div>
+          <div class="work-progress">
+            <div class="work-icon-box ">
+              <span><img src="images/services/air/sanitization.png"></span>
+            </div>
+            <h3 class="caption">Sanitization of tools &amp; area</h3>
+          </div>
+          <div class="work-arrow">
+          </div>
+          <div class="work-progress">
+            <div class="work-icon-box">
+              <span><img src="images/services/air/aarogya.png"></span>
+            </div>
+            <h3 class="caption">Arogya Setu to ensure safety</h3>
+          </div>
+
         </div>
-    </section>
+      </div>
+    </div>
+  </section> -->
 
-    <section class="serv-sec">
-            <div class="serv-main" id="ac">
-                <div class="container">
-                    <div class="main">
-                        <div class="lt">
-                            <img src="images/ac.jpg" alt="" />
-                        </div>
-                        <div class="rt">
-                            <h2>AC Service</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quos aliquam saepe voluptatum libero velit quo, culpa adipisci asperiores odio aliquid dolores fugiat earum facere a omnis eum. Iste, delectus!
-                            Sequi natus quo ducimus molestiae explicabo excepturi nisi, eius, accusantium ipsum adipisci debitis fugiat dolor ex aut harum neque corporis quae sunt itaque sapiente, nobis assumenda laboriosam iure illo? Iusto?</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="serv-main" id="geyser">
-                <div class="container">
-                    <div class="main">
-                        <div class="lt">
-                            <img src="images/geyser.jpg" alt="" />
-                        </div>
-                        <div class="rt">
-                            <h2>AC repairing</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente fuga sit harum nihil. Facilis reprehenderit culpa suscipit, nisi hic optio pariatur placeat, laborum praesentium veniam mollitia nesciunt at laboriosam? Optio!
-                            Quae distinctio pariatur laudantium perspiciatis, dolore aliquid minus quibusdam ut dolores possimus, similique obcaecati quia. Iusto consectetur voluptatibus asperiores, recusandae odit reiciendis, provident culpa unde saepe dicta atque. Placeat, fugiat.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="serv-main" id="microwave">
-                <div class="container">
-                    <div class="main">
-                        <div class="lt">
-                            <img src="images/Microwave.jpg" alt="" />
-                        </div>
-                        <div class="rt">
-                            <h2>Split AC Installation </h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente fuga sit harum nihil. Facilis reprehenderit culpa suscipit, nisi hic optio pariatur placeat, laborum praesentium veniam mollitia nesciunt at laboriosam? Optio!
-                            Quae distinctio pariatur laudantium perspiciatis, dolore aliquid minus quibusdam ut dolores possimus, similique obcaecati quia. Iusto consectetur voluptatibus asperiores, recusandae odit reiciendis, provident culpa unde saepe dicta atque. Placeat, fugiat.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="serv-main" id="fridge">
-                <div class="container">
-                    <div class="main">
-                        <div class="lt">
-                            <img src="images/Refrigerators.jpg" alt="" />
-                        </div>
-                        <div class="rt">
-                            <h2>Wimdow AC Installation</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente fuga sit harum nihil. Facilis reprehenderit culpa suscipit, nisi hic optio pariatur placeat, laborum praesentium veniam mollitia nesciunt at laboriosam? Optio!
-                            Quae distinctio pariatur laudantium perspiciatis, dolore aliquid minus quibusdam ut dolores possimus, similique obcaecati quia. Iusto consectetur voluptatibus asperiores, recusandae odit reiciendis, provident culpa unde saepe dicta atque. Placeat, fugiat.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="serv-main" id="washer">
-                <div class="container">
-                    <div class="main">
-                        <div class="lt">
-                            <img src="images/loading-washer.jpg" alt="" />
-                        </div>
-                        <div class="rt">
-                            <h2>Washing Machine Service</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente fuga sit harum nihil. Facilis reprehenderit culpa suscipit, nisi hic optio pariatur placeat, laborum praesentium veniam mollitia nesciunt at laboriosam? Optio!
-                            Quae distinctio pariatur laudantium perspiciatis, dolore aliquid minus quibusdam ut dolores possimus, similique obcaecati quia. Iusto consectetur voluptatibus asperiores, recusandae odit reiciendis, provident culpa unde saepe dicta atque. Placeat, fugiat.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="serv-main" id="core">
-                <div class="container">
-                    <div class="main">
-                        <div class="lt">
-                            <img src="images/Microwave.jpg" alt="" />
-                        </div>
-                        <div class="rt">
-                            <h2>Core Cutting</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente fuga sit harum nihil. Facilis reprehenderit culpa suscipit, nisi hic optio pariatur placeat, laborum praesentium veniam mollitia nesciunt at laboriosam? Optio!
-                            Quae distinctio pariatur laudantium perspiciatis, dolore aliquid minus quibusdam ut dolores possimus, similique obcaecati quia. Iusto consectetur voluptatibus asperiores, recusandae odit reiciendis, provident culpa unde saepe dicta atque. Placeat, fugiat.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="serv-main" id="amc">
-                <div class="container">
-                    <div class="main">
-                        <div class="lt">
-                            <img src="images/loading-washer.jpg" alt="" />
-                        </div>
-                        <div class="rt">
-                            <h2>A M C</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente fuga sit harum nihil. Facilis reprehenderit culpa suscipit, nisi hic optio pariatur placeat, laborum praesentium veniam mollitia nesciunt at laboriosam? Optio!
-                            Quae distinctio pariatur laudantium perspiciatis, dolore aliquid minus quibusdam ut dolores possimus, similique obcaecati quia. Iusto consectetur voluptatibus asperiores, recusandae odit reiciendis, provident culpa unde saepe dicta atque. Placeat, fugiat.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="serv-main" id="commercial">
-                <div class="container">
-                    <div class="main">
-                        <div class="lt">
-                            <img src="images/mob-banner-1.jpg" alt="" />
-                        </div>
-                        <div class="rt">
-                            <h2>Commercial Service</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente fuga sit harum nihil. Facilis reprehenderit culpa suscipit, nisi hic optio pariatur placeat, laborum praesentium veniam mollitia nesciunt at laboriosam? Optio!
-                            Quae distinctio pariatur laudantium perspiciatis, dolore aliquid minus quibusdam ut dolores possimus, similique obcaecati quia. Iusto consectetur voluptatibus asperiores, recusandae odit reiciendis, provident culpa unde saepe dicta atque. Placeat, fugiat.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </section>
 
-    <?php include('include/footer.php')?>
+  <section class="define">
+    <div class="container">
+      <div class="main_def">
+        <div class="main-left">
+          <div class="repair">
+            <h2>AC Repair Service</h2>
+            <p>Your air conditioner stops functioning often when it runs continuously during summers preventing you from
+              getting cool breeze, in that situation you are going to hire an expert who has expertise in ac repair
+              service and can deal with your ac repairing problem efficiently.</p>
+          </div>
+
+          <div class="repair">
+            <h3>About Ac Repair Service</h3>
+            <p>Air conditioner is basically used in offices and houses in urban areas to keep the hot environment down
+              and get good air to breathe because due to urban pollution it’s almost impossible to get good air without
+              a smoothly functioning ac and because of that there are plenty of domestic and international brands are
+              available in the country.<br><br>
+
+              By booking ac repair service you expect services like cleaning, gas replacement and overhauling done at
+              reasonable cost, however, ac repairing and servicing is a task that cannot be done by an amateur it can
+              only be done by an expert and luckily you are at right spot in the search of an experienced technician for
+              your ac repair service.</p>
+          </div>
+          <div class="repair">
+            <h3>AC Problem:</h3>
+            <ul>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+              <li>Air condition starting problem</li>
+            </ul>
+          </div>
+        </div>
+        <div class="main-right">
+          <div class="repair">
+            <h2>All Brands</h2>
+            <ul>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+            </ul>
+          </div>
+          <div class="repair">
+            <h2>Why Choose us</h2>
+            <ul>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+              <li>Voltas AC repairing and</li>
+
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  </section>
+  <section class="reviews">
+    <div class="container">
+      <div class="main-d">
+        <div class="comm-d">
+          <figure>
+            <img src="images/services/air/Ac-Repair-Service.jpg" alt="">
+          </figure>
+          <div class="low">
+            <h2>Top Ac Repair</h2>
+            <div class="ratings"> <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod similique magni mollitia natus odio
+              deserunt
+              eum tenetur iusto excepturi aliquid nostrum, impedit ipsa eaque quis blanditiis cum ipsum voluptatibus
+              corrupti.</p>
+          </div>
+        </div>
+        <div class="comm-d">
+          <figure>
+            <img src="images/services/air/Ac-Repair-Service.jpg" alt="">
+          </figure>
+          <div class="low">
+            <h2>Top Ac Repair</h2>
+            <div class="ratings"> <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod similique magni mollitia natus odio
+              deserunt
+              eum tenetur iusto excepturi aliquid nostrum, impedit ipsa eaque quis blanditiis cum ipsum voluptatibus
+              corrupti.</p>
+          </div>
+        </div>
+        <div class="comm-d">
+          <figure>
+            <img src="images/services/air/Ac-Repair-Service.jpg" alt="">
+          </figure>
+          <div class="low">
+            <h2>Top Ac Repair</h2>
+            <div class="ratings"> <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod similique magni mollitia natus odio
+              deserunt
+              eum tenetur iusto excepturi aliquid nostrum, impedit ipsa eaque quis blanditiis cum ipsum voluptatibus
+              corrupti.</p>
+          </div>
+        </div>
+        <div class="comm-d">
+          <figure>
+            <img src="images/services/air/Ac-Repair-Service.jpg" alt="">
+          </figure>
+          <div class="low">
+            <h2>Top Ac Repair</h2>
+            <div class="ratings"> <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod similique magni mollitia natus odio
+              deserunt
+              eum tenetur iusto excepturi aliquid nostrum, impedit ipsa eaque quis blanditiis cum ipsum voluptatibus
+              corrupti.</p>
+          </div>
+        </div>
+        <div class="comm-d">
+          <figure>
+            <img src="images/services/air/Ac-Repair-Service.jpg" alt="">
+          </figure>
+          <div class="low">
+            <h2>Top Ac Repair</h2>
+            <div class="ratings"> <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod similique magni mollitia natus odio
+              deserunt
+              eum tenetur iusto excepturi aliquid nostrum, impedit ipsa eaque quis blanditiis cum ipsum voluptatibus
+              corrupti.</p>
+          </div>
+        </div>
+        <div class="comm-d">
+          <figure>
+            <img src="images/services/air/Ac-Repair-Service.jpg" alt="">
+          </figure>
+          <div class="low">
+            <h2>Top Ac Repair</h2>
+            <div class="ratings"> <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod similique magni mollitia natus odio
+              deserunt
+              eum tenetur iusto excepturi aliquid nostrum, impedit ipsa eaque quis blanditiis cum ipsum voluptatibus
+              corrupti.</p>
+          </div>
+        </div>
+    
+      </div>
+    </div>
+  </section>
+
+  <?php include('include/footer.php')?>
 </body>
+
 </html>
